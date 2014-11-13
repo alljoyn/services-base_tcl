cb9ee20645270d1d15b75b29ab6c498af49fbaa3
Aaron Pulliam <aaronp@qce.qualcomm.com>
2014-11-12 16:18:35 -0800

    ASABASE-337: Update ServerSample to allow for some run-time configuration on linux
    Change-Id: Iec06381fca0362399c843e849aa97c5c335ae5b0
    Signed-off-by: Aaron Pulliam <aaronp@qce.qualcomm.com>

--- sample_apps/AppsCommon/target/linux/NotificationProducerSampleUtil.h
+++ sample_apps/AppsCommon/target/linux/NotificationProducerSampleUtil.h
@@ -23,5 +23,9 @@ void Producer_SetupEnv(uint8_t* inputMode);
 void Producer_GetNotificationFromUser(AJNS_NotificationContent* notificationContent, uint16_t* messageType, uint32_t* ttl, uint32_t* nextMessageTime);
 void Producer_GetShouldDeleteNotificationFromUser(AJ_BusAttachment* busAttachment, uint8_t* delMsg, uint16_t* delMsgType);
 void Producer_FreeNotification(AJNS_NotificationContent* notificationContent);
+void Producer_ReadConfigFromEnv(uint8_t* producerEnabled, AJNS_NotificationContent* notificationContent,
+    uint32_t* messagesInterval, uint8_t* intervalTypeRandom, uint32_t* initialIntervalOffset,
+    uint16_t * messagePriority, uint8_t* priorityTypeRandom, uint32_t* messageTtl);
+
 #endif /* PRODUCER_SAMPLE_UTIL_H_ */
 
--- sample_apps/ServerSample/SimpleNotificationProducerSample.c
+++ sample_apps/ServerSample/SimpleNotificationProducerSample.c
@@ -34,6 +34,7 @@
 #define Producer_SetupEnv(...) do { } while (0)
 #define Producer_GetShouldDeleteNotificationFromUser(...) do { } while (0)
 #define Producer_FreeNotification(...) do { } while (0)
+#define Producer_ReadConfigFromEnv(...) do { } while (0)
 #endif
 
 #ifndef NDEBUG
@@ -69,11 +70,18 @@ static AJNS_DictionaryEntry richAudioUrls[NUM_RICH_AUDIO];
 
 static uint8_t inputMode = 0;
 static AJ_Time isMessageTime;
+static uint8_t producerEnabled = 1;
 
 #ifndef MESSAGES_INTERVAL
 #define MESSAGES_INTERVAL 60000
 #endif
+#ifndef MESSAGES_INTERVAL_OFFSET
+#define MESSAGES_INTERVAL_OFFSET 0
+#endif
+
+static uint32_t messagesInterval = MESSAGES_INTERVAL;
 static uint32_t nextMessageTime = MESSAGES_INTERVAL;
+static uint32_t initialIntervalOffset = MESSAGES_INTERVAL_OFFSET;
 
 typedef enum _PriorityType {
     PRIORITY_TYPE_FIXED = 0,
@@ -99,11 +107,17 @@ static const char* const INTERVAL_TYPES[] = { "Fixed", "Random" };
 #ifndef FIXED_MESSAGE_TYPE
 #define FIXED_MESSAGE_TYPE AJNS_NOTIFICATION_MESSAGE_TYPE_INFO
 #endif
+static uint16_t messageType = FIXED_MESSAGE_TYPE;
 static const char* const PRIORITIES[AJNS_NUM_MESSAGE_TYPES] = { "Emergency", "Warning", "Info" };
 
 #ifndef FIXED_TTL
 #define FIXED_TTL AJNS_NOTIFICATION_TTL_MIN // Note needs to be in the range AJNS_NOTIFICATION_TTL_MIN..AJNS_NOTIFICATION_TTL_MAX
 #endif
+
+static uint32_t messageTtl;
+
+static uint32_t remaining_offset = 0;
+static uint8_t haveInitialOffset = 0;
 
 /**
  * Initialize the Notifications that will be sent during this sample app
@@ -111,6 +125,7 @@ static const char* const PRIORITIES[AJNS_NUM_MESSAGE_TYPES] = { "Emergency", "Wa
 static AJNS_NotificationContent notificationContent;
 static void InitNotification()
 {
+
     notificationContent.numCustomAttributes = NUM_CUSTOMS;
     customAttributesToSend[0].key   = onKey;
     customAttributesToSend[0].value = HelloVal;
@@ -136,6 +151,8 @@ static void InitNotification()
     notificationContent.richIconObjectPath = richIconObjectPath;
     notificationContent.richAudioObjectPath = richAudioObjectPath;
     notificationContent.controlPanelServiceObjectPath = controlPanelServiceObjectPath;
+
+    messageTtl = FIXED_TTL;
 }
 
 /**
@@ -146,24 +163,53 @@ AJ_Status NotificationProducer_Init()
     AJ_Status status = AJ_OK;
     uint32_t random;
 
-    Producer_SetupEnv(&inputMode);
     InitNotification();
+    Producer_SetupEnv(&inputMode);
+    if (!inputMode) {
+        uint8_t priorityTypeRandom = (priorityType == PRIORITY_TYPE_RANDOM);
+        uint8_t intervalTypeRandom = (intervalType == INTERVAL_TYPE_RANDOM);
+        Producer_ReadConfigFromEnv(&producerEnabled, &notificationContent, &messagesInterval, &intervalTypeRandom, &initialIntervalOffset,
+                                   &messageType, &priorityTypeRandom, &messageTtl);
+        priorityType = priorityTypeRandom ? PRIORITY_TYPE_RANDOM : PRIORITY_TYPE_FIXED;
+        intervalType = intervalTypeRandom ? INTERVAL_TYPE_RANDOM : INTERVAL_TYPE_FIXED;
+    }
+
     status = AJNS_Producer_Start();
 
     AJ_AlwaysPrintf(("\n---------------------\nNotification Producer started!\n"));
-    AJ_AlwaysPrintf(("Interval:     %u ms\n", MESSAGES_INTERVAL));
+    AJ_AlwaysPrintf(("Interval:     %u ms\n", messagesInterval));
+    AJ_AlwaysPrintf(("Offset:       %u ms\n", initialIntervalOffset));
     AJ_AlwaysPrintf(("IntervalType: %s (%u)\n", INTERVAL_TYPES[intervalType], intervalType));
-    AJ_AlwaysPrintf(("Priority      %s (%u)\n", PRIORITIES[FIXED_MESSAGE_TYPE], FIXED_MESSAGE_TYPE));
+    AJ_AlwaysPrintf(("Priority      %s (%u)\n", PRIORITIES[messageType], messageType));
     AJ_AlwaysPrintf(("PriorityType: %s (%u)\n", PRIORITY_TYPES[priorityType], priorityType));
-    AJ_AlwaysPrintf(("TTL:          %u s\n", FIXED_TTL));
+    AJ_AlwaysPrintf(("TTL:          %u s\n", messageTtl));
+    if (!inputMode) {
+        int8_t i;
+        AJ_AlwaysPrintf(("NumTexts:     %u\n", notificationContent.numTexts));
+        for (i = 0; i < notificationContent.numTexts; i++) {
+            AJ_AlwaysPrintf(("MsgText:  %s (%d)\n", notificationContent.texts->key, i));
+            AJ_AlwaysPrintf(("MsgText:  %s (%d)\n", notificationContent.texts->value, i));
+        }
+    }
     AJ_AlwaysPrintf(("---------------------\n\n"));
     AJ_InitTimer(&isMessageTime);
     if (!inputMode) {
-        if (intervalType == INTERVAL_TYPE_RANDOM) { // Randomize next message time if interval type is RANDOM
-            AJ_RandBytes((uint8_t*)&random, sizeof(random));
-            nextMessageTime = random % MESSAGES_INTERVAL;
+        if (producerEnabled) {
+            nextMessageTime = messagesInterval;
+            if (intervalType == INTERVAL_TYPE_RANDOM) { // Randomize next message time if interval type is RANDOM
+                AJ_RandBytes((uint8_t*)&random, sizeof(random));
+                nextMessageTime = random % messagesInterval;
+            }
+            if (initialIntervalOffset > 0) { // Delay the initial message by a random amount if value is greater than 0
+                AJ_RandBytes((uint8_t*)&random, sizeof(random));
+                initialIntervalOffset = random % initialIntervalOffset;
+                nextMessageTime += initialIntervalOffset;
+                haveInitialOffset = 1;
+            }
+            AJ_AlwaysPrintf(("Next message will be sent in %u ms\n", nextMessageTime));
+        } else {
+            AJ_AlwaysPrintf(("Producer disabled. No messages will be sent\n"));
         }
-        AJ_AlwaysPrintf(("Next message will be sent in %u ms\n", nextMessageTime));
     } else {
         isMessageTime.seconds -= nextMessageTime / 1000; // Expire next message timer
         isMessageTime.milliseconds -= nextMessageTime % 1000; // Expire next message timer
@@ -199,10 +245,9 @@ static void PossiblyDeleteNotification(AJ_BusAttachment* busAttachment)
  */
 static void PossiblySendNotification(AJ_BusAttachment* busAttachment)
 {
-    static uint32_t offset = 0;
     AJ_Status status;
     uint16_t messageType = FIXED_MESSAGE_TYPE;
-    uint32_t ttl = FIXED_TTL;
+    uint32_t ttl = messageTtl;
     uint32_t serialNum;
     uint32_t random;
     uint32_t elapsed = AJ_GetElapsedTime(&isMessageTime, TRUE);
@@ -224,14 +269,18 @@ static void PossiblySendNotification(AJ_BusAttachment* busAttachment)
             PossiblyDeleteNotification(busAttachment);
         }
         if (!inputMode) {
+            if (haveInitialOffset) {     // clear this and set nextMessageTime back to the default
+                haveInitialOffset = 0;
+                nextMessageTime = messagesInterval;
+            }
             if (intervalType == INTERVAL_TYPE_RANDOM) { // Randomize next message time if interval type is RANDOM
                 AJ_RandBytes((uint8_t*)&random, sizeof(random));
-                if (elapsed < (MESSAGES_INTERVAL + offset)) {
-                    offset += MESSAGES_INTERVAL - elapsed;
+                if (elapsed < (messagesInterval + remaining_offset)) {
+                    remaining_offset += messagesInterval - elapsed;
                 } else {
-                    offset = 0; // If we stalled too much simply randomize from now and cleared carried over offset
+                    remaining_offset = 0;     // If we stalled too much simply randomize from now and cleared carried over offset
                 }
-                nextMessageTime = offset + (random % MESSAGES_INTERVAL); // randomize within the next time window of MESSAGES_INTERVAL
+                nextMessageTime = remaining_offset + (random % messagesInterval); // randomize within the next time window of messagesInterval
             }
         }
         AJ_AlwaysPrintf(("Next message will be sent in %u ms\n", nextMessageTime));
